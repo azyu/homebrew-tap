ac7debfd79cd4877e033621f32de4b8274390020
Wondoo Kang <azyu@live.com>
2026-06-28 01:42:06 +0900

    kis 0.1.2

--- kis.rb
+++ kis.rb
@@ -1,24 +1,24 @@
 class Kis < Formula
   desc "KIS Open API CLI for Korea Investment & Securities"
   homepage "https://github.com/azyu/kis-cli"
-  version "0.1.1"
+  version "0.1.2"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/azyu/kis-cli/releases/download/v0.1.1/kis_0.1.1_macos_arm64.tar.gz"
-      sha256 "ab8b32847560174c20a31736dc11f7422b68e1b092ab0cbf5d85bd4427a1c415"
+      url "https://github.com/azyu/kis-cli/releases/download/v0.1.2/kis_0.1.2_macos_arm64.tar.gz"
+      sha256 "c84bdbef91d87712e464f500c6d36787df30830d2123682885a6957137471270"
     end
   end
 
   on_linux do
     if Hardware::CPU.intel?
-      url "https://github.com/azyu/kis-cli/releases/download/v0.1.1/kis_0.1.1_linux_amd64.tar.gz"
-      sha256 "c0f89837f89ad988089d6dbe2a08a1ff3437c9b612979fea42335ec048b5c15e"
+      url "https://github.com/azyu/kis-cli/releases/download/v0.1.2/kis_0.1.2_linux_amd64.tar.gz"
+      sha256 "efbdd3737a6b11976785fad6778c83a01b13e76318e76847fede361e4d914f84"
     end
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/azyu/kis-cli/releases/download/v0.1.1/kis_0.1.1_linux_arm64.tar.gz"
-      sha256 "53bce6f0262cb6a074c17166736fa7222d45b004638abaf00e9dcb37680d5383"
+      url "https://github.com/azyu/kis-cli/releases/download/v0.1.2/kis_0.1.2_linux_arm64.tar.gz"
+      sha256 "3608f742e1acf9d1b9b204377f4f34ca3cd7ddb279d1ad53b3114c217314bf81"
     end
   end
 
